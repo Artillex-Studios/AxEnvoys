5182e4b225a6c8db742879c5a6ec331f521b18e8
BenceX100 <52270269+BenceX100@users.noreply.github.com>
2023-04-08 11:49:17 +0200

    Update BlockPhysicsListener.java

--- src/main/java/me/bencex100/rivalsenvoy/listeners/BlockPhysicsListener.java
+++ src/main/java/me/bencex100/rivalsenvoy/listeners/BlockPhysicsListener.java
@@ -1,6 +1,7 @@
 package me.bencex100.rivalsenvoy.listeners;
 
 import me.bencex100.rivalsenvoy.utils.EnvoyHandler;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPhysicsEvent;
@@ -9,8 +10,12 @@ public class BlockPhysicsListener implements Listener {
 
     @EventHandler
     public void onForm(BlockPhysicsEvent e) {
-        if (!EnvoyHandler.isActive()) return;
-        if (!EnvoyHandler.getCenter().getWorld().equals(e.getBlock().getWorld())) return;
-        e.setCancelled(true);
+        if (EnvoyHandler.isActive()) {
+            if (e.getBlock().getType() == Material.OCHRE_FROGLIGHT || e.getBlock().getType() == Material.PEARLESCENT_FROGLIGHT || e.getBlock().getType() == Material.VERDANT_FROGLIGHT) {
+                if (EnvoyHandler.getCenter().getWorld().equals(e.getBlock().getWorld())) {
+                    e.setCancelled(true);
+                }
+            }
+        }
     }
 }
