38b274862c88fe7203adc33e747d54f09d2e4e2b
TomTom <93038247+AverageGithub@users.noreply.github.com>
2024-04-04 11:12:25 +0200

    feat: add support for itemsadder furnitures

--- src/main/java/com/artillexstudios/axenvoy/integrations/blocks/impl/ItemsAdderBlockIntegration.java
+++ src/main/java/com/artillexstudios/axenvoy/integrations/blocks/impl/ItemsAdderBlockIntegration.java
@@ -2,25 +2,38 @@ package com.artillexstudios.axenvoy.integrations.blocks.impl;
 
 import com.artillexstudios.axenvoy.integrations.blocks.BlockIntegration;
 import dev.lone.itemsadder.api.CustomBlock;
+import dev.lone.itemsadder.api.CustomFurniture;
 import org.bukkit.Location;
 
+@SuppressWarnings("UnreachableCode")
 public class ItemsAdderBlockIntegration implements BlockIntegration {
 
     @Override
     public void place(String id, Location location) {
         String blockId = id.substring("itemsadder:".length());
-        CustomBlock block = CustomBlock.getInstance(blockId);
+        if (CustomBlock.isInRegistry(blockId)) {
+            CustomBlock block = CustomBlock.getInstance(blockId);
 
-        if (block != null) {
-            block.place(location);
+            if (block != null) {
+                block.place(location);
+            }
+        } else if (CustomFurniture.isInRegistry(blockId)) {
+            CustomFurniture.spawn(blockId, location.getBlock());
         }
     }
 
     @Override
     public void remove(Location location) {
         CustomBlock block = CustomBlock.byAlreadyPlaced(location.getBlock());
-        if (block == null) return;
+        if (block != null) {
+            block.remove();
+            return;
+        }
 
-        block.remove();
+        CustomFurniture furniture = CustomFurniture.byAlreadySpawned(location.getBlock());
+
+        if (furniture != null) {
+            furniture.remove(false);
+        }
     }
 }
