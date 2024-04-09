2e4a6bc85fc6c140a0cf27858efb7b7802692e1b
TomTom <93038247+AverageGithub@users.noreply.github.com>
2024-04-09 18:34:22 +0200

    fix: try fixing running envoys not being removed on server stop

--- src/main/java/com/artillexstudios/axenvoy/listeners/WorldLoadListener.java
+++ src/main/java/com/artillexstudios/axenvoy/listeners/WorldLoadListener.java
@@ -1,14 +1,30 @@
 package com.artillexstudios.axenvoy.listeners;
 
+import com.artillexstudios.axenvoy.envoy.Envoy;
 import com.artillexstudios.axenvoy.envoy.Envoys;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldLoadEvent;
+import org.bukkit.event.world.WorldUnloadEvent;
 
 public class WorldLoadListener implements Listener {
 
     @EventHandler
     public void onWorldLoad(WorldLoadEvent event) {
         Envoys.reload();
+    }
+
+    @EventHandler
+    public void onWorldUnloadEvent(WorldUnloadEvent event) {
+        for (Envoy envoy : Envoys.getTypes().values()) {
+            if (!envoy.isActive()) continue;
+
+            World world = envoy.getCenter().getWorld();
+            if (world == null) continue;
+            if (event.getWorld().equals(world)) {
+                envoy.stop();
+            }
+        }
     }
 }
