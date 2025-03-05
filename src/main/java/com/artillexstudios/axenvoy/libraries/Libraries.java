4179b81c432153f8de8d1899828b86e60c5705e7
TomTom <93038247+AverageGithub@users.noreply.github.com>
2025-03-05 15:58:27 +0100

    fix: fix an error with library loading

--- src/main/java/com/artillexstudios/axenvoy/libraries/Libraries.java
+++ src/main/java/com/artillexstudios/axenvoy/libraries/Libraries.java
@@ -5,8 +5,7 @@ import revxrsal.zapper.Dependency;
 public enum Libraries {
     SLF4J("org.slf4j:slf4j-api:2.0.9"),
     COMMONS_IO("commons-io:commons-io:2.15.0"),
-    COMMONS_TEXT("org{}apache{}commons:commons-text:1.11.0"),
-    MATH_3("org{}apache{}commons:commons-math3:3.6.1");
+    COMMONS_TEXT("org{}apache{}commons:commons-text:1.11.0");
 
     private final Dependency library;
 
